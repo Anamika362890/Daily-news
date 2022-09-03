b2c2ed1aaa09048b799d2d38823f803c1b67f507
unknown <anamika362890@gmail.com>
2022-09-03 17:10:53 +0600

    modal

--- JS/app.js
+++ JS/app.js
@@ -48,11 +48,11 @@ const displayNews = async category_id => {
 
 
 }
-const DetailsNews = categories => {
-    //console.log(categories);
+const DetailsNews = newses => {
+    //console.log(newses);
     const newsContainer = document.getElementById('news-container');
-    categories.forEach(catagory => {
-        console.log(catagory);
+    newses.forEach(news => {
+        console.log(news);
 
 
         const newsDiv = document.createElement('div');
@@ -65,26 +65,26 @@ const DetailsNews = categories => {
     <div class="card mb-3 border p-3" >
     <div class="row g-0">
       <div class="col-md-4 main">
-        <img src="${catagory.image_url}" class="img-fluid rounded-start" alt="...">
+        <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">${catagory.title}</h5>
-          <p class="card-text">${catagory.details.slice(0, 600)}....</p>
+          <h5 class="card-title">${news.title}</h5>
+          <p class="card-text">${news.details.slice(0, 600)}....</p>
           <div class="d-flex">
           <div class="author me-3">
-          <img src="${catagory.author.img}" class="  img-fluid rounded-circle " alt="...">
+          <img src="${news.author.img ? news.author.img : 'No Images Found'}" class="  img-fluid rounded-circle " alt="...">
           </div>
           <div>
-          <h6>${catagory.author.name} </h6>
-          <p>${catagory.author.published_date}</p>
+          <h6>${news.author.name ? news.author.name : 'No name Found'} </h6>
+          <p>${news.author.published_date ? news.author.published_date : 'No Date Found'}</p>
           </div>
           <div class="d-flex align-items-center align-items-center mx-auto">
           <div class="icon ">
           <img src="view.png" class=" profile " alt="...">
           </div>
           <div class="ms-4">
-          <h5>${catagory.total_view}</h5>
+          <h5>${news.total_view ? news.total_view : 'No Viewers'}</h5>
           </div>
         </div>
         <div class="text-muted mx-auto mt-3">
@@ -99,8 +99,13 @@ const DetailsNews = categories => {
 
     </div>
     <div>
-    <button  type="button" class="btn btn-info mx-auto mt-3 py-2 px-2">See Details</button>
+    <button onclick="displayNewsDetails('${news._id}')" type="button" class="btn btn-info mx-auto mt-3 py-2 px-2" data-bs-toggle="modal" data-bs-target="#exampleModal">See Details</button>
+
+
     <div>
+
+
+    
 
 
       </div>
@@ -115,6 +120,34 @@ const DetailsNews = categories => {
 }
 
 
-loadNewsCategory();
+const displayNewsDetails = async news_id => {
 
+
+    const url = ` https://openapi.programming-hero.com/api/news/${news_id}`;
+    //console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    DetailsNewsButton(data.data[0])
+
+}
+
+const DetailsNewsButton = info => {
+    console.log(info);
+    const modalTitle = document.getElementById('exampleModalLabel');
+    modalTitle.innerText = info.title;
+    const Details = document.getElementById('modal-details');
+    Details.innerHTML = `
+    <p>Author Name:${info.author.name ? info.author.name : 'No author name found'}</p>
+
+    <p>Rating Badge:${info.rating?.badge ? info.rating.badge : 'No Rating Found'}</p>
+
+    <p>Viewers:${info.total_view ? info.total_view : 'No viewers Found'}</p >
+
+        `;
+
+
+
+
+}
+loadNewsCategory();
 

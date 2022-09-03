d04fd3e9008bac5111a7cdc838b5da2ff7cb7fd0
unknown <anamika362890@gmail.com>
2022-09-03 20:40:29 +0600

    search

--- JS/app.js
+++ JS/app.js
@@ -17,9 +17,9 @@ const displayNewsCategory = categories => {
         const catagoryDiv = document.createElement('div');
         catagoryDiv.classList.add('col');
         catagoryDiv.innerHTML = `
-        <h6 onclick="displayNews('${category.category_id}')" >
-        ${category.category_name}
-        </h6>
+
+        <a onclick="displayNews('${category.category_id}')" class="h6  gray text-decoration-none" href="#"> ${category.category_name}</a>
+        
 
 
 
@@ -63,43 +63,44 @@ const DetailsNews = newses => {
 
 
     <div class="card mb-3 border p-3" >
-    <div class="row g-0">
+    <div class="row g-3">
       <div class="col-md-4 main">
         <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">${news.title}</h5>
-          <p class="card-text">${news.details.slice(0, 600)}....</p>
-          <div class="d-flex">
-          <div class="author me-3">
+          <h5 class="card-title h3 mb-4">${news.title}</h5>
+          <p class="card-text gray">${news.details.slice(0, 600)}....</p>
+          <div class="d-flex mt-4">
+          <div class="author  me-3">
           <img src="${news.author.img ? news.author.img : 'No Images Found'}" class="  img-fluid rounded-circle " alt="...">
           </div>
           <div>
           <h6>${news.author.name ? news.author.name : 'No name Found'} </h6>
-          <p>${news.author.published_date ? news.author.published_date : 'No Date Found'}</p>
+          <p class="gray">${news.author.published_date ? news.author.published_date : 'No Date Found'}</p>
           </div>
           <div class="d-flex align-items-center align-items-center mx-auto">
           <div class="icon ">
           <img src="view.png" class=" profile " alt="...">
           </div>
-          <div class="ms-4">
-          <h5>${news.total_view ? news.total_view : 'No Viewers'}</h5>
+          <div class="ms-4 gray mt-1 ">
+          <h5 class="fw-bold">${news.total_view ? news.total_view : 'No Viewers'}</h5>
           </div>
-        </div>
+        </div >
         <div class="text-muted mx-auto mt-3">
-        <i class="fa-solid fa-star empty"></i>
-        <i class="fa-solid fa-star filled"></i>
-        <i class="fa-solid fa-star filled"></i>
-        <i class="fa-solid fa-star filled"></i>
-        <i class="fa-solid fa-star filled"></i>
+        <i class="fa-solid fa-star-half-stroke"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+ 
+        <i class="fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
         
         
 
 
     </div>
     <div>
-    <button onclick="displayNewsDetails('${news._id}')" type="button" class="btn btn-info mx-auto mt-3 py-2 px-2" data-bs-toggle="modal" data-bs-target="#exampleModal">See Details</button>
+    <button onclick="displayNewsDetails('${news._id}')" type="button" class="btn btn-info mx-auto mt-2 py-2 px-2 text-white fw-bold" data-bs-toggle="modal" data-bs-target="#exampleModal">See Details</button>
 
 
     <div>

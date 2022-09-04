34bd0cc8279a2c882d1ac469db95c682992b0421
unknown <anamika362890@gmail.com>
2022-09-04 10:46:50 +0600

    try

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,14 @@
 
 const loadNewsCategory = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-    displayNewsCategory(data.data.news_category)
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsCategory(data.data.news_category)
+    }
+    catch (error) {
+        console.log('Something Wrong')
+    }
 
 
 }
@@ -57,6 +61,8 @@ const displayNews = async category_id => {
 
 }
 const DetailsNews = newses => {
+
+
     console.log(newses);
     newses.sort(function (a, b) {
         return b.total_view - a.total_view
